61abb0748718b708d578b53b2dc0b6862d33ed07
Svetlana Isaykina <svet4731@gmail.com>
2023-01-20 17:09:40 +0300

    Задача № 2

--- task_3_2.py
+++ task_3_2.py
@@ -1,2 +1,15 @@
 # Задача 2. В файле находятся названия фруктов.Выведите все фрукты, названия которых начинаются на заданную букву.
 # а–>абрикос,авокадо, апельсин, айва.
+
+data = open('fruits_list.txt', encoding= 'utf-8')
+text = data.readlines()
+data.close
+
+letter = input('Задайте букву: ')
+temp = []
+
+for el in text:
+    if el[0].casefold() == letter.casefold():
+        temp.append(el)
+print(f'{letter.upper()} -> {temp}')
+
