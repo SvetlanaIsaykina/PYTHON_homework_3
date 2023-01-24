e6dd09008f166227b851bbf35563fa3e6bc68460
Svetlana Isaykina <svet4731@gmail.com>
2023-01-24 12:54:42 +0300

    extra

--- task_3_3.py
+++ task_3_3.py
@@ -2,17 +2,46 @@
 # Бот должен, как минимум, отвечать на фразы «привет», «как тебя зовут». 
 # Если фраза ему неизвестна, он выводит соответствующую фразу.
 
-bot_script = {
-    'привет': 'Добрый день!',
-    'как тебя зовут': 'Меня зовут Алина',
-    'кто ты': 'Я твой помощник',
-    'чем ты можешь мне помочь': 'Объясню основы PYTHON',
+def set_phrase_list():
+    data = open('phrase.txt', mode= 'r', encoding= 'utf - 8')
+    phrase_list = data.readlines()
+    data.close()
+    dictionary = {}
+    for phrase in phrase_list:
+        phrase = phrase.removesuffix('\n')
+        phrase = phrase.split(': ')
+        dictionary[phrase[0]] = phrase[1]
+    return dictionary
 
-}
+bot_script = set_phrase_list()
 
-user_text = input('Задайте вопрос чат-боту: ')
+# # МОЙ ВАРИАНТ
+# user_text = input('Задайте вопрос чат-боту: ')
 
-if user_text in bot_script:
-    print(bot_script[user_text])
-else:
-    print('Я не понимаю')
+# if user_text in bot_script:
+#     print(bot_script[user_text])
+# else:
+#     print('Я не понимаю')
+
+# ДРУГОЙ ВАРИАНТ
+
+start_dialog = True
+
+while start_dialog:
+    phrase = input('Я: ')
+    phrase = phrase.lower()
+    if phrase in bot_script.keys():
+        print(f'Бот: {bot_script[phrase]}')
+    else:
+        print(f'я не понимаю')
+        check = input('Бот: скажешь мне правильный ответ (Y/N)?\nЯ: ')
+        check = check.lower()
+        if check == 'y':
+            answer = input(f'Бот: как ты ответишь на "{phrase}"?\nЯ: ')
+            data = open('phrase.txt', mode= 'a', encoding= 'utf-8')
+            data.write(f'{phrase}: {answer}\n')
+            data.close()
+            bot_script[phrase] = answer
+            # bot_script = set_phrase_list()
+    if phrase == 'выход':
+        start_dialog = False
